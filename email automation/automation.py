8d0466abf47840dba830fd5b5ed1bac83ac2893d
Javed Chaudhary <jachoudhary@oneshield.com>
2023-11-25 13:00:40 +0530

    added try catch, removed unwanted code

--- email automation/automation.py
+++ email automation/automation.py
@@ -3,60 +3,54 @@ import ssl
 from email.message import EmailMessage
 import sys
 
-class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
-
 def send_mail(name ,recipient):
-    # Set your email credentials and details
-    sender_email = "codemonkeyservices@gmail.com"
-    sender_password = "xvti dfry ijzy nips"
-    customer_email= recipient 
-    print(f"sending mail to: {recipient}\n" )
+    try:
+        # Set your email credentials and details
+        sender_email = "codemonkeyservices@gmail.com"
+
+        # read readme.txt to understand how to generate password token for your gmail address
+        sender_password = "<gmail token password>"
+        customer_email= recipient 
+        print(f"sending mail to: {recipient}\n" )
 
 
-    # Create the email message
-    subject = "CodeMonkey - Your Gateway to a Captivating Hotel Website!"
+        # Create the email message
+        subject = "CodeMonkey - Your Gateway to a Captivating Hotel Website!"
 
+            
         
-    
-    em = EmailMessage()
-    em["From"] = sender_email
-    em["To"] = recipient
-    em["Subject"] = subject
+        em = EmailMessage()
+        em["From"] = sender_email
+        em["To"] = recipient
+        em["Subject"] = subject
 
 
-#    getting mail content from htmlMailFormat.txt file 
-    mailContent = ''
-    with open('htmlMailFormat.txt', 'r') as f:
-        mailContent=f.read()
+    #    getting mail content from htmlMailFormat.txt file 
+        mailContent = ''
+        with open('htmlMailFormat.txt', 'r') as f:
+            mailContent=f.read()
 
-    mailContent=mailContent.replace("__name__", name)
+        mailContent=mailContent.replace("__name__", name)
 
-    # mail content 
-    html_body = f"""
-              {mailContent}
-    """
+        # mail content 
+        html_body = f"""
+                {mailContent}
+        """
 
-    em.add_alternative(html_body , subtype='html')
+        em.add_alternative(html_body , subtype='html')
 
-  # Add SSL (layer of security)
-    context = ssl.create_default_context()
+    # Add SSL (layer of security)
+        context = ssl.create_default_context()
 
 
-        # Connect to the SMTP server
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-        smtp.login(sender_email, sender_password)
-        smtp.sendmail(sender_password, customer_email, em.as_string())
-    print("Email sent successfully!")
+            # Connect to the SMTP server
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(sender_email, sender_password)
+            smtp.sendmail(sender_password, customer_email, em.as_string())
+        print("Email sent successfully!")
+
+    except:
+        print(f"failed to send mail to: {customer_email}")
 
     
 
